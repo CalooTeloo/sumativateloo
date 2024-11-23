46bed2220b3f4db6e9eed76d4a9fac0559c456ee
calichitello <calichitello@gmail.com>
2024-11-23 18:53:00 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ViewComponent } from './Components/view/view.component';
+import { ViewComponent } from './Components/Padre/view/view.component';
 
 @Component({
   selector: 'app-root',
